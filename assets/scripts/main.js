5b3e1bb31904ea7aba2a58674bd760607babf697
Александр Кустов <aleksandr@MBP-Aleksandr.Dlink>
2020-11-10 22:29:01 +0300

    secondeCommite

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,5 @@
 array = [{name: '', age: ''}, {name: '', age: ''},{name: '', age: ''},{name: '', age: ''}];
 
-document.getElementById('js-button_1').onclick = collect;
-function collect() {
-	array[0].name = prompt('Введите имя');
-	array[0].age = +prompt('Введите возраст');
-	if (array[0].age < 7 || !array[0].age) {
-		alert('Nononnonononononono');
-		array[0].age = +prompt('Введите возраст');
-	} else {
-		console.log(array[0]);
-	}
-};
-
 document.getElementById('js-button_2').onclick = collect1;
 function collect1() {
 	array[1].name = prompt('Введите имя');
@@ -45,5 +33,17 @@ function collect2() {
 		array[2].age = +prompt('Введите возраст');
 	} else {
 		console.log(array[2]);
+	}
+};
+
+document.getElementById('js-button_1').onclick = collect;
+function collect() {
+	array[0].name = prompt('Введите имя');
+	array[0].age = +prompt('Введите возраст');
+	if (array[0].age < 7 || !array[0].age) {
+		alert('Nononnonononononono');
+		array[0].age = +prompt('Введите возраст');
+	} else {
+		console.log(array[0]);
 	}
 };
